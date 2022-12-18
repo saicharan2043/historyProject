0f04396020338e57fa4f1f20a6066346e8c95ab1
Saicharan2043 <saikanna2043@gmail.com>
2022-12-18 20:36:39 +0000

    save code

--- src/Components/SearchItem/index.js
+++ src/Components/SearchItem/index.js
@@ -13,14 +13,14 @@ const SearchItem = props => {
       <div className="domine-container">
         <p className="time-text">{timeAccessed}</p>
         <div className="img-container">
+          <img src={logoUrl} alt="domain logo" className="img-logo" />
           <div className="logo-img-container">
-            <img src={logoUrl} alt="domain logo" className="img-logo" />
-            <p className="time-tex">{title}</p>
+            <p className="time-text">{title}</p>
+            <p className="domine-name time-text">{domainUrl}</p>
           </div>
-          <p className="domine-name time-text">{domainUrl}</p>
         </div>
       </div>
-      <button onClick={deleteClick} className="button">
+      <button onClick={deleteClick} className="button" testid="delete">
         <img
           src="https://assets.ccbp.in/frontend/react-js/delete-img.png"
           alt="delete"
